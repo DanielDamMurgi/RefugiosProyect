38c484ae02a077bd607015760796fc199021c59e
Equipo <daniel.ruedalao@gmail.com>
2018-09-29 12:37:41 +0200

    27/09/2018

--- app/src/main/java/com/example/equipo/refugiosproyect/SierraFragment.java
+++ app/src/main/java/com/example/equipo/refugiosproyect/SierraFragment.java
@@ -1,14 +1,19 @@
 package com.example.equipo.refugiosproyect;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
-public class SierraFragment extends Fragment {
+public class SierraFragment extends Fragment implements View.OnClickListener{
+
+    private CardView infoCard;
 
     public SierraFragment() {
         // Required empty public constructor
@@ -20,6 +25,24 @@ public class SierraFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_sierra, container, false);
 
+        infoCard = view.findViewById(R.id.cardView_infoSierra);
+        infoCard.setOnClickListener(this);
+
+
         return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i;
+
+
+        switch (v.getId()){
+            case R.id.cardView_infoSierra:
+                Toast.makeText(getActivity(),"llega",Toast.LENGTH_SHORT).show();
+                i = new Intent(getActivity(),InfoActivity.class);
+                startActivity(i);
+                break;
+        }
     }
 }
