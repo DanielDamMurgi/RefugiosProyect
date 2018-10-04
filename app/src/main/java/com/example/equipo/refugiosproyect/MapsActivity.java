58a3c6f4b745c5544e44c7e69da773f3005b4109
Equipo <daniel.ruedalao@gmail.com>
2018-10-04 22:07:33 +0200

    04/10/2018

--- app/src/main/java/com/example/equipo/refugiosproyect/MapsActivity.java
+++ app/src/main/java/com/example/equipo/refugiosproyect/MapsActivity.java
@@ -1,39 +1,29 @@
 package com.example.equipo.refugiosproyect;
 
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.media.Image;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
-import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.places.Places;
-import com.google.android.gms.location.places.ui.PlacePicker;
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 
@@ -49,11 +39,9 @@ public class MapsActivity extends FragmentActivity implements
     private Criteria criteria;
     private String proveedor = "";
     private GoogleMap mapa;
-    private LatLng ubeire = new LatLng(37.1185152,-2.9021601);
+    private LatLng ubeire = new LatLng(37.1185152, -2.9021601);
     private boolean primera = true;
     private FloatingActionButton floatingActionButton;
-
-
 
     //IMPLEMENTACION
 
@@ -87,7 +75,7 @@ public class MapsActivity extends FragmentActivity implements
                 //AÑADIMOS LAS APIS QUE NOS INTERESAN
                 .addApi(LocationServices.API)
                 .addApi(Places.GEO_DATA_API)
-                .enableAutoManage(this,this)
+                .enableAutoManage(this, this)
                 .build();
 
 
@@ -155,7 +143,7 @@ public class MapsActivity extends FragmentActivity implements
             //btnMiPos.setEnabled(false);
         }
 
-        manejador.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,2000000,200,this);
+        manejador.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 2000000, 200, this);
         mapa.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
         añadirMarcadores(primera);
 
@@ -176,16 +164,19 @@ public class MapsActivity extends FragmentActivity implements
 
     }
 
-    private void añadirMarcadores(Boolean primera){
+    private void añadirMarcadores(Boolean primera) {
 
-        mapa.addMarker(new MarkerOptions()
+        Marker marker = mapa.addMarker(new MarkerOptions()
                 .position(ubeire)
                 .title("Ubeire")
                 .icon(BitmapDescriptorFactory
                         .fromResource(android.R.drawable.ic_menu_compass))
                 .anchor(0.5f, 0.5f));
 
+        marker.showInfoWindow();
+
         mapa.addMarker(new MarkerOptions().position(sierraCoor).title("Sierra Nevada"));
+
 
         if (primera == true) {
             mapa.moveCamera(CameraUpdateFactory.newLatLng(sierraCoor));
