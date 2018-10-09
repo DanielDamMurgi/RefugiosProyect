40b5ae4bfe266053215e3fa7020bbbf12b3f6045
Equipo <daniel.ruedalao@gmail.com>
2018-10-09 11:43:31 +0200

    04/10/2018

--- app/src/main/java/com/example/equipo/refugiosproyect/FotosRefugioFragment.java
+++ app/src/main/java/com/example/equipo/refugiosproyect/FotosRefugioFragment.java
@@ -4,9 +4,13 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.equipo.refugiosproyect.ClasesPrincipales.Imagen;
 
 public class FotosRefugioFragment extends Fragment {
 
@@ -19,6 +23,14 @@ public class FotosRefugioFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_fotos_refugio, container, false);
+
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getContext(),2);
+        RecyclerView recyclerView = view.findViewById(R.id.rv_fotosRefugios);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(layoutManager);
+
+        ImagenAdapter adapter = new ImagenAdapter(getContext(), Imagen.getImagenes());
+        recyclerView.setAdapter(adapter);
 
         return  view;
     }
