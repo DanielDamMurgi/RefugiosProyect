767c6e32f3e30c47b3613b8b0a8767d2fb5612c3
Equipo <daniel.ruedalao@gmail.com>
2018-11-04 13:32:50 +0100

    18/10/2018

--- app/src/main/java/com/example/equipo/refugiosproyect/weather/WeatherActivity.java
+++ app/src/main/java/com/example/equipo/refugiosproyect/weather/WeatherActivity.java
@@ -1,15 +1,67 @@
 package com.example.equipo.refugiosproyect.weather;
 
+import android.Manifest;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.example.equipo.refugiosproyect.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
+import java.util.List;
+
 
 public class WeatherActivity extends AppCompatActivity {
+
+    //ATRIBUTOS
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    private CoordinatorLayout coordinatorLayout;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private LocationCallback locationCallback;
+    private LocationRequest locationRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+
+        coordinatorLayout = findViewById(R.id.coordinator_weather);
+        toolbar = findViewById(R.id.toolbar_weather);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //PERMISOS
+        Dexter.withActivity(this)
+                .withPermissions(Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.ACCESS_FINE_LOCATION)
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport report) {
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+                        Snackbar.make(coordinatorLayout,"Permisos denegados",Snackbar.LENGTH_LONG)
+                                .show();
+                    }
+                }).check();
     }
+
+
 }
--- app/src/main/java/com/example/equipo/refugiosproyect/weather/model/Coord.java
+++ app/src/main/java/com/example/equipo/refugiosproyect/weather/model/Coord.java
@@ -28,4 +28,9 @@ public class Coord {
     public void setLat(double lat) {
         this.lat = lat;
     }
+
+    @Override
+    public String toString(){
+        return new StringBuilder("[").append(this.lat).append(',').append(this.lon).append(']').toString();
+    }
 }
